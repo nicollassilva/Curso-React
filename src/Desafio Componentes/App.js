0cf2ddb838e77dfd3638363ebc7b7275e121642c
Nícollas Silva <lyod.hp@gmail.com>
2020-07-23 15:35:00 -0300

    Desafio componentes

---
+++ src/Desafio Componentes/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+import Produtos from './Produtos';
+
+const produtos = [
+  { nome: 'Notebook', propriedades: ['16gb ram', '512gb'] },
+  { nome: 'Smartphone', propriedades: ['2gb ram', '128gb'] },
+];
+
+const App = () => {
+  const { pathname } = window.location;
+
+  let Component;
+  if (pathname === '/produtos') {
+    Component = Produtos;
+  } else {
+    Component = Home;
+  }
+
+  return (
+    <section>
+      <Header />
+      <Component />
+    </section>
+  );
+};
+
+export default App;
